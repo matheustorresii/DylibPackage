01fd14cb2e3f558d554387af5cbbf4fc67e0dab3
Matheus Torres <matheustorresii@Matheuss-MacBook-Pro.local>
2024-07-06 01:38:28 -0300

    Create project

---
+++ Sources/DylibPackage/DylibPackage.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+public struct DynamicView: View {
+    public init() { }
+    
+    public var body: some View {
+        Text("Hello from Dynamic Library")
+            .padding()
+    }
+}
