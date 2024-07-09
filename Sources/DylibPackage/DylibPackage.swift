cb590819589aa23a6297dbef17c19085fbcc6b9f
matheustorresii <matheustf2003@gmail.com>
2024-07-08 22:12:45 -0300

    update real time

--- Sources/DylibPackage/DylibPackage.swift
+++ Sources/DylibPackage/DylibPackage.swift
@@ -4,7 +4,7 @@ public struct DynamicView: View {
     public init() { }
     
     public var body: some View {
-        Text("ATUALIZAÇÃO OTA")
+        Text("DEU CERTO ESSA BOMBA")
             .padding()
     }
 }
